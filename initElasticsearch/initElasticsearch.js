8441fcb469b06ea06fc52cbadb68bb9af15f7e48
Xiaoxi <yxx0827@gmail.com>
2021-09-03 00:42:58 -0400

    use bulk save

--- initElasticsearch/initElasticsearch.js
+++ initElasticsearch/initElasticsearch.js
@@ -71,6 +71,39 @@ const saveDocToES = async (doc) => {
   }
 }
 
+const bulkSave = async (array) => {
+  try {
+    const body = array.flatMap(doc => [
+      {
+        index: {
+          _index: INDEX_NAME,
+          _id: `${doc._id}`
+        }
+      },
+      {
+        source: doc.source.name,
+        author: doc.author,
+        title: doc.title,
+        description: doc.description,
+        publishedAt: doc.publishedAt,
+        content: doc.content,
+        category: doc.category,
+        url: doc.url,
+        urlToImage: doc.urlToImage
+      }
+    ])
+    await client.bulk({
+      refresh: false,
+      body
+    })
+    return true
+  } catch (e) {
+    console.log(e)
+    console.log('failed save: ', array[0]._id, array[array.length - 1]._id);
+    return false
+  }
+}
+
 const connectToMongo = () => {
   return new Promise((resolve, reject) => {
     mongoose.connect(process.env.MONGODB_URI, {
@@ -96,30 +129,75 @@ const connectToMongo = () => {
 (async () => {
   let saved = 0;
   let failedID = [];
+  let bulkArray = [];
+  const startedAt = Date.now();
 
   try {
     await createIndex();
     const mongo = await connectToMongo();
-    const cursor = await mongo.collection('news').find();
+    const cursor = await mongo.collection('news').find().sort({"_id":1});
 
+    // disable index refresh
+    await client.indices.put_settings({
+      index: INDEX_NAME,
+      body: {
+        refresh_interval: -1
+      }
+    })
+
+    // save one by one
+    // const handleOne = async () => {
+    //   const doc = await cursor.next();
+    //   const isSaved = await saveDocToES(doc);
+    //   isSaved ? saved++ : failedID.push(`${doc._id}`);
+    //   if (saved % 1000 === 0) {
+    //     console.log('saved: ', saved)
+    //   }
+    //   const hasNext = await cursor.hasNext();
+    //   if (hasNext) {
+    //     await handleOne()
+    //   }
+    //   return true
+    // }
+    // await handleOne();
+
+    // save bulk
     const handleOne = async () => {
       const doc = await cursor.next();
-      const isSaved = await saveDocToES(doc);
-      isSaved ? saved++ : failedID.push(`${doc._id}`);
-      if (saved % 1000 === 0) {
+      bulkArray.push(doc);
+      saved++;
+      const hasNext = await cursor.hasNext();
+      if (bulkArray.length === 5000 || !hasNext) {
+        await bulkSave(bulkArray);
+        bulkArray = [];
         console.log('saved: ', saved)
       }
-      const hasNext = await cursor.hasNext();
       if (hasNext) {
         await handleOne()
       }
       return true
     }
     await handleOne();
+
+    // refresh after all saved
+    await client.indices.refresh({
+      index: INDEX_NAME
+    });
+
+    // restore normal refresh interval
+    await client.indices.put_settings({
+      index: INDEX_NAME,
+      body: {
+        refresh_interval: '1s'
+      }
+    })
+
+    return true
   } catch (e) {
     console.log(e)
   } finally {
     console.log('saved: ', saved);
-    console.log('failed: ', failedID)
+    console.log('failed: ', failedID);
+    console.log('time elapsed in seconds: ', Math.round((Date.now() - startedAt) / 1000))
   }
 })()
